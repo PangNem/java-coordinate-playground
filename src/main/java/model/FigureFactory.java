67d80cf49943f0617c50724a2b0a7e849ebccefe
pangnem <sprnd645@gmail.com>
2022-03-07 23:39:45 +0900

    refactor: 다형성 활용해 if문 제거

--- src/main/java/model/FigureFactory.java
+++ src/main/java/model/FigureFactory.java
@@ -1,17 +1,25 @@
 package model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class FigureFactory {
+    private static final Map<Integer, Function<List<Point>, Figure>> classifier = new HashMap<>();
+
+    static {
+        classifier.put(2, Line::new);
+        classifier.put(4, Rectangle::new);
+    }
+
     public static Figure create(List<Point> points) {
-        if (points.size() == 2) {
-            return new Line(points);
+        int size = points.size();
+        if (size == 3 || size < 2 || size > 4) {
+            throw new IllegalArgumentException("허용되지 않은 좌표 허용값입니다.");
         }
 
-        if (points.size() == 4) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("허용되지 않은 좌표 허용값입니다.");
+        return classifier.get(size)
+                .apply(points);
     }
 }
